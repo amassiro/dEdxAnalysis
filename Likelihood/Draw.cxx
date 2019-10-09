8175bb42b451a5c35b1a59ff7b90f717d8f3d918
amassiro <massironi.andrea@gmail.com>
2019-10-09 14:22:22 +0200

    up

--- Likelihood/Draw.cxx
+++ Likelihood/Draw.cxx
@@ -7,9 +7,48 @@ void Draw(std::string nameFile) {
   
   TTree* tree = (TTree*) fileIn->Get("tree");
   
+   
+  TCanvas* mycc2 = new TCanvas ("mycc2", "", 800, 600);
+  
+  //   
+  //   https://root.cern.ch/root/html/tutorials/fit/langaus.C.html
+  //   
+  //   
+  //   //par[0]=Width (scale) parameter of Landau density
+  //   //par[1]=Most Probable (MP, location) parameter of Landau density
+  //   //par[2]=Total area (integral -inf to inf, normalization constant)
+  //   //par[3]=Width (sigma) of convoluted Gaussian function
+  //   
+//   
+//   
+//   EXT PARAMETER                                   STEP         FIRST   
+//   NO.   NAME      VALUE            ERROR          SIZE      DERIVATIVE 
+//   1  p0           2.23944e-01   1.15281e-03   3.17857e-06   8.26056e-02
+//   2  p1           2.41976e+00   1.44900e-03   4.77642e-06  -1.57049e-01
+//   3  p2           1.04642e-01   2.01036e-04   7.70407e-07   5.29265e-01
+//   4  p3           3.53386e-01   2.09308e-03   5.31332e-06   8.05870e-03
+//   
   
   
-//   tree->Draw ("IsoTrack_dedxByHit1[best_track]", "(IsoTrack_layerOrSideByHit1[best_track] == 1) && (IsoTrack_ladderOrBladeByHit1[best_track] == 1) && (IsoTrack_pixByHit1[best_track] == 1 ) && ( (run >= 320673) && (run < 325173)  ) && ( (  abs(IsoTrack_eta[best_track] ) >=0.000000) && (  abs(IsoTrack_eta[best_track] ) <2.500000)   ) " )
+  TF1 *function2 = new TF1("function2",langaufun, 1, 7, 4);
+  
+  function2->SetParameters(0.22, 2.42, 0.1, 0.35);
+  function2->SetLineColor(kBlue);
+  function2->SetLineStyle(2);
+  function2->SetLineWidth(2);
+  
+  function2->Draw("PL");
+  
+  TCanvas* mycc = new TCanvas ("mycc", "", 800, 600);
+  
+  tree->Draw ("IsoTrack_dedxByHit1[best_track]", "(IsoTrack_layerOrSideByHit1[best_track] == 1) && (IsoTrack_ladderOrBladeByHit1[best_track] == 1) && (IsoTrack_pixByHit1[best_track] == 1 ) && ( (  abs(IsoTrack_eta[best_track] ) >=0.000000) && (  abs(IsoTrack_eta[best_track] ) <2.500000)   ) " );
+  
+  
+  
+  TCanvas* mycc3 = new TCanvas ("mycc3", "", 800, 600);
+
+
+  tree->Draw ("langaus(IsoTrack_dedxByHit1[best_track])", "(IsoTrack_layerOrSideByHit1[best_track] == 1) && (IsoTrack_ladderOrBladeByHit1[best_track] == 1) && (IsoTrack_pixByHit1[best_track] == 1 ) && ( (  abs(IsoTrack_eta[best_track] ) >=0.000000) && (  abs(IsoTrack_eta[best_track] ) <2.500000)   ) " );
   
   
 }
--- Likelihood/langaus.C
+++ Likelihood/langaus.C
@@ -1,5 +1,79 @@
 
+
+float langaus(float x) {
+  
+  
+  
+  //Fit parameters:
+  //par[0]=Width (scale) parameter of Landau density
+  //par[1]=Most Probable (MP, location) parameter of Landau density
+  //par[2]=Total area (integral -inf to inf, normalization constant)
+  //par[3]=Width (sigma) of convoluted Gaussian function
+  //
+  //In the Landau distribution (represented by the CERNLIB approximation),
+  //the maximum is located at x=-0.22278298 with the location parameter=0.
+  //This shift is corrected within this function, so that the actual
+  //maximum is identical to the MP parameter.
+  
+  Double_t par[4];
+  par[0] = 0.22;
+  par[1] = 2.42;
+  par[2] = 0.1;
+  par[3] = 0.35;
+
+  
+  // Numeric constants
+  Double_t invsq2pi = 0.3989422804014;   // (2 pi)^(-1/2)
+  Double_t mpshift  = -0.22278298;       // Landau maximum location
+  
+  // Control constants
+  Double_t np = 100.0;      // number of convolution steps
+  Double_t sc =   5.0;      // convolution extends to +-sc Gaussian sigmas
+  
+  // Variables
+  Double_t xx;
+  Double_t mpc;
+  Double_t fland;
+  Double_t sum = 0.0;
+  Double_t xlow,xupp;
+  Double_t step;
+  Double_t i;
+  
+  
+  // MP shift correction
+  mpc = par[1] - mpshift * par[0];
+  
+  // Range of convolution integral
+  xlow = x - sc * par[3];
+  xupp = x + sc * par[3];
+  
+  step = (xupp-xlow) / np;
+  
+  // Convolution integral of Landau and Gaussian by sum
+  for(i=1.0; i<=np/2; i++) {
+    xx = xlow + (i-.5) * step;
+    fland = TMath::Landau(xx,mpc,par[0]) / par[0];
+    sum += fland * TMath::Gaus(x,xx,par[3]);
+    
+    xx = xupp - (i-.5) * step;
+    fland = TMath::Landau(xx,mpc,par[0]) / par[0];
+    sum += fland * TMath::Gaus(x,xx,par[3]);
+  }
+  
+  return (par[2] * step * sum * invsq2pi / par[3]);
+  
+}
+
+
+
+
+
+
+
+
 Double_t langaufun(Double_t *x, Double_t *par) {
+  // Double_t langaus(Double_t *x, Double_t *par) {
+  
   
   
   //Fit parameters:
